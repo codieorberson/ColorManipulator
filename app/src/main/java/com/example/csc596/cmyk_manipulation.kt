2dbc90ebf6a266049d3b18199308f6b3d0597dd4
Codie Orberson <31399048+codieorberson@users.noreply.github.com>
2020-04-09 08:11:04 -0500

    Add functionality to seekbars
    Sorry the code is sloppy right now. Since we have so many seekbars, there has to be a function for each one of them

--- app/src/main/java/com/example/csc596/cmyk_manipulation.kt
+++ app/src/main/java/com/example/csc596/cmyk_manipulation.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.fragment_cmyk_manipulation.*
+
+
 
 class cmyk_manipulation : Fragment() {
 
@@ -13,7 +16,22 @@ class cmyk_manipulation : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cmyk_manipulation, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_cmyk_manipulation, container, false)
+
+        val seek1 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar1)
+        val seek2 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar2)
+        val seek3 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar3)
+        val seek4 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar4)
+        val seek5 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar5)
+        val seek6 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar6)
+        val seek7 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar7)
+        val seek8 = view.findViewById<SeekBar>(R.id.cmyk_as_seekBar8)
+
+        val box1 = view.findViewById<View>(R.id.cmykbox1)
+        val box2 = view.findViewById<View>(R.id.cmykbox2)
+
+        return view
     }
 
 }
